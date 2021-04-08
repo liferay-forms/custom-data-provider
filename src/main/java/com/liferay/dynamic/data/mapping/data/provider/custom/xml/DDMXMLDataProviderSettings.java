5472122ac465b139cdc2a6199f25239cf4c3cd41
Carolina <carolina.barbosa@liferay.com>
2021-04-08 08:51:18 -0300

    LPS-129191 Create a custom data provider that retrieves data in XML format

---
+++ src/main/java/com/liferay/dynamic/data/mapping/data/provider/custom/xml/DDMXMLDataProviderSettings.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.data.provider.custom.xml;
+
+import com.liferay.dynamic.data.mapping.annotations.DDMForm;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormField;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayout;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutColumn;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutPage;
+import com.liferay.dynamic.data.mapping.annotations.DDMFormLayoutRow;
+import com.liferay.dynamic.data.mapping.data.provider.DDMDataProviderParameterSettings;
+
+/**
+ * @author Carolina Barbosa
+ */
+@DDMForm
+@DDMFormLayout(
+	{
+		@DDMFormLayoutPage(
+			{
+				@DDMFormLayoutRow(
+					{
+						@DDMFormLayoutColumn(
+							size = 12, value = {"url", "outputParameters"}
+						)
+					}
+				)
+			}
+		)
+	}
+)
+public interface DDMXMLDataProviderSettings
+	extends DDMDataProviderParameterSettings {
+
+	@DDMFormField(
+		label = "%url", required = true,
+		validationErrorMessage = "%please-enter-a-valid-url",
+		validationExpression = "isURL(url)"
+	)
+	public String url();
+
+}
